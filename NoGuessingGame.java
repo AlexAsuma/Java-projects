5cd6be2b899c0f1b6fb0c0ed094ad73c98ccb432
asuma <alexasuma.tech@gmail.com>
2026-09-19 19:54:13 +0300

    Nineth commit

--- NoGuessingGame.java
+++ NoGuessingGame.java
@@ -6,14 +6,32 @@ public class NoGuessingGame {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int guess, attempts,
-                randomNumber = random.nextInt(1, 11);
+        int guess, attempts = 0,
+
+                min = 1,
+                max = 100,
+                randomNumber = random.nextInt(min, max + 1);
 
         System.out.println("*******************************************");
         System.out.println("***********NUMBER GUESSING GAME************");
         System.out.println("*******************************************");
 
-        
+        do {
+            System.out.printf("Guess a number between %d-%d\n", min, max);
+            guess = scanner.nextInt();
+            attempts++;
+            if (guess < randomNumber) {
+                System.out.println("TOO LOW!! Try again");
+
+            } else if (guess > randomNumber) {
+                System.out.println("TOO HIGH!!Try again");
+            } else {
+                System.out.println("CORRECT! The number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+
+        } while (guess != randomNumber);
+        System.out.println("*******************************************");
 
         scanner.close();
     }
