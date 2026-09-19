e241482e21e2089cf8304d8565bfe803fb7e5b2a
asuma <alexasuma.tech@gmail.com>
2026-09-19 20:10:47 +0300

    Tenth commit

---
+++ OverloadedMethods.java
@@ -0,0 +1,14 @@
+public class OverloadedMethods {
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
