92f142f1554ee315c2a5145f3c8f5c899eaa35c8
asuma <alexasuma.tech@gmail.com>
2026-09-23 11:37:08 +0300

    another commit 3

---
+++ OOP/Main.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Main {
+       
+public static void main(String[] args){
+    Student student1 = new Student("James", 30, 3.8);
+    Student student2 = new Student("Clark", 25, 3.6);
+
+    System.out.println(student1.name);
+    System.out.println(student2.name);
+}
+}
